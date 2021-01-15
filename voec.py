0bd30f47c9f42ecb481eb94cab118f250d907e9c
= <thibaud.freyd@gmail.com>
2021-01-15 17:19:15 +0100

    issue with double selection, need to find a way to reset either country or company selection, need to find a way to force the display with function

--- voec.py
+++ voec.py
@@ -14,75 +14,65 @@ Source: [Skatteetaten](https://www.skatteetaten.no/globalassets/bedrift-og-organ
 
 
 """)
-st.sidebar.write('''** Author: [Thibaud Freyd](https://www.linkedin.com/in/thibaud-freyd/)**
+st.sidebar.write('''** Author: [Thibaud Freyd](https://www.linkedin.com/in/thibaud-freyd/)**''')
 
-Version 1.1 ''')
+st.sidebar.write('''Version 1.1
+
+*known issue*: need to refresh the page to reset everything ''')
 #loading of the data
 @st.cache
 def import_data():
     df = pd.read_csv('./list_companies_extraction/list_complete.csv')
     df = df.sort_values('country')
     
-
     #generation of the list of countries
-
     all_countries = df['country'].unique().astype(str)
     all_countries = sorted(all_countries)
-    all_countries.append('')
-
+    all_countries.insert(0,'')
     
     #list of all companies 
     all_companies = df['company'].unique()
     all_companies = sorted(all_companies)
-    all_companies.append('')
-
+    all_companies.insert(0,'')
 
     return df, all_countries, all_companies
 
+
+
 df, all_countries,all_companies=  import_data()
+
+
 
 #double columns
 left_col_selection,right_col_selection = st.beta_columns(2)
 
 #initiation index
-index_country=len(all_countries)-1
-index_company=len(all_companies)-1
+index_country=0
+index_company=0
 
 # box for country selection
-country_selection=left_col_selection.selectbox("Country : ",all_countries,index=index_country,format_func=lambda x: 'Select an option' if x == '' else x)
-
+country_selection=left_col_selection.selectbox("Country : ",all_countries,index=0,format_func=lambda x: 'Select an option' if x == '' else x)
 #box for company selection
-company_selection=right_col_selection.selectbox("Company: ", all_companies,index=index_company,format_func=lambda x: 'Select an option' if x == '' else x)
+company_selection = right_col_selection.selectbox("Company: ", all_companies,index=index_company,format_func=lambda x: 'Select an option' if x == '' else x)
 
-# #button country selection
+
+#button country selection
 # if left_col_selection.button('Valid',key='country_button'):
 #     left_col_selection.write(f'{country_selection} selected')
 #     df =df.loc[df['country']==country_selection]
 #     df=df.sort_values('company') 
-
+#     st.write(df)
 
 if country_selection or company_selection:
+    if country_selection:
+        st.success(f' "{country_selection}" selected')
+        st.write(df.loc[df['country']==country_selection])
     if company_selection:
         st.success(f' "{company_selection}" selected')
-        df =df.loc[df['company']==company_selection]
-    elif country_selection:
-        st.success(f' "{country_selection}" selected')
-        df =df.loc[df['country']==country_selection]
-        
-else:
-    st.warning('Please select a country or a company')
+        st.write(df.loc[df['company']==company_selection])
 
 
-# #company button validation
-# if right_col_selection.button('Valid',key='company_button'):
-#     st.success('Valided')
-#     df =df.loc[df['company']==company_selection]
-
-
-st.dataframe(df)
 st.write('''*Database from 27.11.20*''')
-
-
 
 #df = user_input_features() #for version with sidebar
 #for version with sidebar
